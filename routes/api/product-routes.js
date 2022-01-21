7ac9a007bc33d418decbf73eac8e65d83aa55c8b
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 18:02:22 -0500

    Added delete for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,14 +30,6 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  /* req.body should look like this...
-    {
-      product_name: "Basketball",
-      price: 200.00,
-      stock: 3,
-      tagIds: [1, 2, 3, 4]
-    }
-  */
   try {
     const productData = await Product.create(req.body);
     res.status(200).json(productData);
@@ -111,6 +103,20 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  try {
+    const productData = await Product.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    (!locationData)
+    ? res.status(404).json({ message: 'No product found with this id.'})
+    : res.status(200).json(productData);
+
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
