ca63119daf904839331b9da9a562b53f580154ff
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 18:45:02 -0500

    Added update

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -35,7 +35,7 @@ router.get('/:id', (req, res) => {
 router.post('/', (req, res) => {
   // create a new tag
   try {
-    tagData = await Tag.create(req.body);
+    const tagData = await Tag.create(req.body);
     res.status(200).json(tagData);
   } catch (err) {
     res.status(400).json(err);
@@ -44,6 +44,19 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
+    });
+
+    (!tagData[0])
+    ? res.status(404).json({ message: 'No tag found with this id.' })
+    : res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
