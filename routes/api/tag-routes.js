f4f3c469525b0c235ee48827f714dc6f5ab5daca
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 19:49:51 -0500

    Revert "Changed put request to match the provided put request for products but replaced values for tags"
    This reverts commit 0ff77890aef1546f7aad86f6f61a40eefcf5d939.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -44,43 +44,19 @@ router.post('/', (req, res) => {
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
-  Tag.update(req.body, {
-    where: {
-      id: req.params.id,
-    },
-  })
-    .then((tag) => {
-      // find all associated products from ProductTag
-      return ProductTag.findAll({ where: { tag_id: req.params.id } });
-    })
-    .then((products) => {
-      // get list of current productIds
-      const taggedProductIds = products.map(({ product_id }) => product_id);
-      // create filtered list of new product_ids
-      const newProducts = req.body.productIds
-        .filter((product_id) => !taggedProductIds.includes(product_id))
-        .map((product_id) => {
-          return {
-            tag_id: req.params.id,
-            product_id,
-          };
-        });
-      // figure out which ones to remove
-      const productToRemove = products
-        .filter(({ product_id }) => !req.body.productIds.includes(product_id))
-        .map(({ id }) => id);
-
-      // run both actions
-      return Promise.all([
-        ProductTag.destroy({ where: { id: productToRemove } }),
-        ProductTag.bulkCreate(newProducts),
-      ]);
-    })
-    .then((updatedProducts) => res.json(updatedProducts))
-    .catch((err) => {
-      // console.log(err);
-      res.status(400).json(err);
+  try {
+    const tagData = await Tag.update(req.body, {
+      where: {
+        id: req.params.id
+      }
     });
+
+    (!tagData[0])
+    ? res.status(404).json({ message: 'No tag found with this id.' })
+    : res.status(200).json(tagData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
