06a8214dfbb8be69f41827ad0e672bbae2e016fd
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 21:11:28 -0500

    Changes include names to be in line with examples

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }]
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'product_tags' }]
+      include: [{ model: Category }, { model: Tag, through: ProductTag, as: 'tags' }]
     });
 
     (!productData)
