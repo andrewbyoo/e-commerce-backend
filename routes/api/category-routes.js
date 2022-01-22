2b27a0557d0f195a014e5f875aab96b7998db093
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 20:03:39 -0500

    Added create

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -17,7 +17,6 @@ router.get('/', (req, res) => {
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findByPk(req.params.id, {
       include: [{ model: Product }]
@@ -33,6 +32,12 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  };
 });
 
 router.put('/:id', (req, res) => {
