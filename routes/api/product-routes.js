4fb23e10ac67e6d0b4ae0e812bb06685466e25b2
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 19:24:05 -0500

    Deleted code as the create code was already provided

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -34,13 +34,6 @@ router.get('/:id', (req, res) => {
 
 // create new product
 router.post('/', (req, res) => {
-  try {
-    const productData = await Product.create(req.body);
-    res.status(200).json(productData);
-  } catch (err) {
-    res.status(400).json(err);
-  };
-
   Product.create(req.body)
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
