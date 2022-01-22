a60da218c873471c499d8478614aee47f62b40d4
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-21 20:02:12 -0500

    Added find by id

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,7 +5,6 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
-  // be sure to include its associated Products
   try {
     const categoryData = await Category.findAll({
       include: [{ model: Product }]
@@ -19,6 +18,17 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    (!categoryData)
+      ? res.status(404).json({ message: 'No category found with this id.' })
+      : res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  };
 });
 
 router.post('/', (req, res) => {
