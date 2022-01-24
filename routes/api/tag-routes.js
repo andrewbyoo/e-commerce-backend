b5958d3ef35c0eb566ac1dd660f2a5e222c05183
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-24 13:17:19 -0500

    Added category onto the includes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { Tag, Product, ProductTag } = require('../../models');
+const { Tag, Category, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all tags
   try {
     const tagData = await Tag.findAll({
-      include: [{ model: Product, through: { ProductTag, attributes: ['id', 'product_id', 'tag_id'] }, as: 'products' }]
+      include: [{ model: Product, through: { ProductTag, attributes: ['id', 'product_id', 'tag_id'] }, as: 'products', include: { model: Category, attributes: ['category_name'] }}]
     });
     res.status(200).json(tagData);
   } catch (err) {
@@ -19,7 +19,7 @@ router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   try {
     const tagData = await Tag.findByPk(req.params.id, {
-      include: [{ model: Product, through: { ProductTag, attributes: ['id', 'product_id', 'tag_id'] }, as: 'products' }]
+      include: [{ model: Product, through: { ProductTag, attributes: ['id', 'product_id', 'tag_id'] }, as: 'products', include: { model: Category, attributes: ['category_name'] } }]
     });
 
     (!tagData)
