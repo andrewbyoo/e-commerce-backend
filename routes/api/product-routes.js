bb44a267e6ea9faae79947e60d68e15bf8cc283f
Andrew Yoo <andrewbyoo@gmail.com>
2022-01-24 10:12:12 -0500

    Changed through include to only show snake cased data and id

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -8,7 +8,7 @@ router.get('/', async (req, res) => {
   // find all products
   try {
     const productData = await Product.findAll({
-      include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, through: ProductTag, as: 'tags' }]
+      include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, through: { ProductTag, attributes: ['id', 'product_id', 'tag_id'] }, as: 'tags' }]
     });
     res.status(200).json(productData);
   } catch (err) {
@@ -21,7 +21,7 @@ router.get('/:id', async (req, res) => {
   // find a single product by its `id`
   try {
     const productData = await Product.findByPk(req.params.id, {
-      include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, through: ProductTag, as: 'tags' }]
+      include: [{ model: Category, attributes: ['category_name'] }, { model: Tag, through: { ProductTag, attributes: ['id', 'product_id', 'tag_id'] }, as: 'tags' }]
     });
 
     (!productData)
